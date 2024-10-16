476a82a3bf18d946b4046509b3dc3cc100669a44
Artur <artur2005456@gmail.com>
2024-10-16 15:56:07 +0300

    add homepage [2]

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return 'Hello, world!'
+  return 'Hello, world!', 200
 
-if __name__ == '__main__':
-  app.run(debug=True)
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+
+    return "This is your homepage :) "
+
+if __name__ == "__main__":
+    app.run()  # Launch built-in web server and run this Flask webapp, debug=True
