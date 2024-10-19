d8b5761d523b6f942a9de899e29ea397dad4e08a
Artur <artur2005456@gmail.com>
2024-10-20 00:31:19 +0300

    add route for resume

--- app.py
+++ app.py
@@ -44,5 +44,9 @@ def detail_post(id):
     return render_template("detail_post.html", post=post)
 
 
+@app.route('/resume')
+def resume():
+  return render_template("resume.html")
+
 if __name__ == "__main__":
     app.run()  # Launch built-in web server and run this Flask webapp, debug=True
