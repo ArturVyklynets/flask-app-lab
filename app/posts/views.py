776c36903ff5600e87dd5ec25fd3f0dc983ab822
Artur <artur2005456@gmail.com>
2024-11-11 23:53:33 +0200

    add functions to save and get posts in json

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,5 +1,6 @@
+import json
 from . import post_bp
-from flask import render_template, abort, flash, redirect, url_for
+from flask import render_template, abort, flash, redirect, url_for, session
 from .forms import PostForm
 posts = [
     {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
@@ -7,25 +8,59 @@ posts = [
     {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
 ] 
 
+POSTS_FILE = './app/static/posts/posts.json'
+
+def load_posts():
+    try:
+        with open(POSTS_FILE, 'r') as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+def save_posts(posts):
+    with open(POSTS_FILE, 'w') as f:
+        json.dump(posts, f, indent=4)
+
+
 @post_bp.route('/add_post', methods=['GET', 'POST'])
 def add_post():
   form = PostForm()
   if form.validate_on_submit():
       title = form.title.data
       content = form.content.data
-      print(title, content)
-      flash(f'Post {title} added successfully!', 'success')
-      return redirect(url_for('.add_post'))
+      category = form.category.data
+      is_active = form.is_active.data
+      publish_date = form.publish_date.data
+      author = session.get('username', 'Anonymous')
+      current_posts = load_posts()
+      new_id = current_posts[-1]['id'] + 1 if current_posts else 1
+      new_post = {
+            "id": new_id,
+            "title": title,
+            "content": content,
+            "category": category,
+            "is_active": is_active,
+            "publication_date": publish_date,
+            "author": author
+      }
+
+      current_posts.append(new_post)
+      save_posts(current_posts)
+
+      flash(f'Пост "{title}" успішно додано!', 'success')
+      return redirect(url_for('.get_posts'))
 
   return render_template('add_post.html', form=form)
 
 @post_bp.route('/') 
 def get_posts():
-    return render_template("posts.html", posts=posts)
+    all_posts = load_posts()
+    return render_template("posts.html", posts=all_posts)
 
 @post_bp.route('/<int:id>') 
 def detail_post(id):
-    if id > 3:
+    all_posts = load_posts()
+    post = next((post for post in all_posts if post["id"] == id), None)
+    if post is None:
         abort(404)
-    post = posts[id-1]
     return render_template("detail_post.html", post=post)
--- app/views.py
+++ app/views.py
@@ -12,6 +12,10 @@ def home():
 
     return render_template("home.html", agent=agent)
 
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html'), 404
+
 @app.route('/resume')
 def resume():
   return render_template("resume.html")
