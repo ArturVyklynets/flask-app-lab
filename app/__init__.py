beb32bdbf6ed4237fa7d0db20b89fda01211f199
Artur <artur2005456@gmail.com>
2024-11-13 16:42:54 +0200

    update config

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
-app = Flask(__name__)
-app.config.from_pyfile("../config.py")
 
-from . import views
+def create_app():
+  app = Flask(__name__)
+  app.config.from_object("config")
 
-from app.posts import post_bp
-app.register_blueprint(post_bp)
+  with app.app_context():
+    from . import views
 
-from app.users import user_bp
-app.register_blueprint(user_bp)
+    from app.posts import post_bp
+    app.register_blueprint(post_bp)
+
+    from app.users import user_bp
+    app.register_blueprint(user_bp)
+  return app
--- app/posts/__init__.py
+++ app/posts/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-post_bp = Blueprint("posts", __name__, url_prefix="/post", template_folder="templates/posts")
+post_bp = Blueprint("posts", __name__, url_prefix="/post", template_folder="templates/posts", static_folder='static')
 
 from . import views
